1975cdc58318518d65d507324f2ea8b4daf6a7c4
AnshikaNautiyal1 <anshika.nautiyal2005@gmail.com>
2026-04-27 11:06:39 +0530

    port changed to 5001

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const db = require("./db");
 require("dotenv").config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 const JWT_SECRET = process.env.JWT_SECRET || "default_secret";
 
 app.use(cors());
